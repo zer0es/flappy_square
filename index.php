1ae0b812c07df611046d351a775625bd894a71e4
zer0es <thanhdat142857@gmail.com>
2014-09-08 15:19:59 +0700

    Flappy Square

---
+++ index.php
@@ -0,0 +1,10 @@
+<object id="player1" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="800" height="450" name="player1">
+  <param name="movie" value="file.swf" />
+  <param name="allowfullscreen" value="true" />
+  <param name="scale" value="noScale" />
+  <param name="salign" value="lt" />
+  <param name="allowscriptaccess" value="always" />
+  <param name="flashvars" value="path=deuter" /> 
+  <embed 
+	id="player1" width="750" height="400" src="file.swf" flashvars="path=deuter" allowfullscreen="true" allowscriptaccess="always" name="player1" scale="noScale" salign="lt">
+  </embed>
